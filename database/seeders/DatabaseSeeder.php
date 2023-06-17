e131ca38bbce6f1cc52e2708b4089208256d297d
triadi <triadilaksamana@gmail.com>
2023-06-17 22:02:08 +0700

    fix: configure is admin in factory and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'isAdmin' => true
         ]);
 
         $this->call([CategorySeeder::class, BookSeeder::class]);
