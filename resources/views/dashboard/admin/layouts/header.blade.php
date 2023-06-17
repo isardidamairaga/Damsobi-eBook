438707c7607929953901c2ff41e66ff8c45cd7b9
Isardi Dama Iraga <isardidamairaga27@gmail.com>
2023-06-17 19:11:07 +0700

    add header

---
+++ resources/views/dashboard/admin/layouts/header.blade.php
@@ -0,0 +1,25 @@
+            <div class="top">
+                <button id="menu-btn">
+                    <span class="material-symbols-sharp active">
+                        menu
+                    </span>
+                </button>
+                <div class="theme-togler">
+                    <span class="material-symbols-sharp active">
+                        light_mode
+                    </span>
+                    <span class="material-symbols-sharp">
+                        dark_mode
+                    </span>
+                </div>
+                <div class="profile">
+                    <div class="info">
+                        <p>Hey, <b>Isardi Dama Iraga</b>
+                            <small class="text-muted">Admin</small>
+                        </p>
+                    </div>
+                    <div class="profile-photo">
+                        <img src="{{ asset('images/cover.jpeg') }}" alt="">
+                    </div>
+                </div>
+            </div>
