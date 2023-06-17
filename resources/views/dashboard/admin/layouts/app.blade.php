3317171c05b2ccf8d41966f4d293643d594fb6f0
Isardi Dama Iraga <isardidamairaga27@gmail.com>
2023-06-17 19:10:57 +0700

    add asset css

--- resources/views/dashboard/admin/layouts/app.blade.php
+++ resources/views/dashboard/admin/layouts/app.blade.php
@@ -10,7 +10,7 @@
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href={{ asset('css/sidebar.css') }} rel="stylesheet">
+    <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
 </head>
 
 <body>
@@ -28,7 +28,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
         integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
     </script>
-    <script src={{ asset('js/sidebars.js') }}></script>
+    <script src="{{ asset('js/index.js')}}"></script>
 </body>
 
 </html>
