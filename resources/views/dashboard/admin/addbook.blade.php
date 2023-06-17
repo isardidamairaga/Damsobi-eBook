482d56baeee7ac7bf5714fe3e3e0e9ebd283d4ec
Isardi Dama Iraga <isardidamairaga27@gmail.com>
2023-06-17 20:46:20 +0700

    edit button

--- resources/views/dashboard/admin/addbook.blade.php
+++ resources/views/dashboard/admin/addbook.blade.php
@@ -1,11 +1,17 @@
-<x-admin-layout title="Upload">
+<x-admin-layout title="List Book">
     @section('container')
     <main>
         <h1>List Book</h1>
         <div class="recent-login">
-            <button class="btn addbook"><span class="material-symbols-sharp">
-                    add
-                </span>Add New Book</button>
+
+            <a href="/dashboard/upload">
+                <div class="btn addbook">
+                    <span class="material-symbols-sharp add">
+                        add
+                    </span>Add New Book
+                </div>
+            </a>
+
             <table class="book">
                 <thead>
                     <tr>
@@ -19,9 +25,9 @@
                 <tbody>
                     @for ($i = 1; $i < 10; $i++) <tr>
                         <td>{{ $i }}</td>
-                        <td>Isardi Dama Iraga</td>
-                        <td>isardidamairaga27@gmail.com</td>
-                        <td class="success">Active</td>
+                        <td>Kambing jantan</td>
+                        <td>fiksi</td>
+                        <td>isardi dama iraga</td>
                         <td>
                             <div class="d-flex action">
                                 <a href=""><span class="material-symbols-sharp edit">
