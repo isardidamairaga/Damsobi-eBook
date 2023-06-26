46b9bffa99d59552fa3e992571554dac8b009b64
isardidamairaga <isardidamairaga27@gmail.com>
2023-06-26 07:00:16 +0700

    Add :  Paginatior for bootstrap

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Paginator::useBootstrapFive();
+        Paginator::useBootstrapFour();
         Schema::defaultStringLength(191);
     }
 }
