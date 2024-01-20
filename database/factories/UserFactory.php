cefcaf8b0556e425c0e6b646a8feac042cca0a82
Isardi Dama Iraga <isardidamairaga27@gmail.com>
2024-01-20 15:20:31 +0700

    database

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,12 +18,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => 'admin',
+            'email' => 'admin@karanganom.ids',
             'email_verified_at' => now(),
-            'username' => \fake()->userName(),
+            'username' => 'admin',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'isAdmin' => false,
+            'isAdmin' => true,
             'remember_token' => Str::random(10),
         ];
     }
